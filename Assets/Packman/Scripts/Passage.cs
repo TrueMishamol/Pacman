960a4ea3794bd924e2abdfff5ae28c80b5f1e776
Mikhail Molodozhenov (Mishamol) <mishamol@bk.ru>
2023-09-30 10:13:41 +0300

    Passage

---
+++ Assets/Packman/Scripts/Passage.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Passage : MonoBehaviour {
+
+
+    public Transform connection;
+
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        Vector3 position = collider.transform.position;
+        position.x = this.connection.position.x;
+        position.y = this.connection.position.y;
+        collider.transform.position = position;
+    }
+}
